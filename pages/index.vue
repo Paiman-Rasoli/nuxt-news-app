8135486ffcde1bf36ad4cfa08b929a85d6dbed85
paiman <paimanrasoli789@gmail.com>
2021-06-19 15:49:54 +0430

    get 10 articles per page from an API for index

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,28 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        test
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="row">
+      <Post
+        v-for="article in articles"
+        :key="article.title"
+        :postProp="article"
+      />
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  // use this for using SSR(server side rendering)
+  async asyncData() {
+    const apiKey = "9e500acbec4c45b6b0ecbb132de0d02c";
+
+    const posts = await fetch(
+      `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${apiKey}`
+    ).then(data => data.json());
+    //get 9 articles
+    const articles = posts.articles.slice(0, 9);
+    return { articles };
+  }
+};
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style scoped></style>
